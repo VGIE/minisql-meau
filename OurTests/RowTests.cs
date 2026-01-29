61427041f740637cc3993c3bafab6c52e68cb38e
Usuario <endika.ladislao@zaraobe.net>
2026-01-29 11:49:06 +0100

    ColumnDefinitionTest and GetAndSetValueTest

--- OurTests/RowTests.cs
+++ OurTests/RowTests.cs
@@ -1,3 +1,5 @@
+using DbManager;
+
 namespace OurTests
 {
     public class RowTests
@@ -10,5 +12,25 @@ namespace OurTests
 
         }
         */
+        [Fact]
+        public void GetValueAndSetValueTest()
+        {
+            List<ColumnDefinition> columns = new List<ColumnDefinition>()
+            {
+                new ColumnDefinition(ColumnDefinition.DataType.String, "Name"),
+                new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"),
+            };
+            List<string> rowValues = new List<string>()
+            {
+                "Jacinto", "37"
+            };
+            Row testRow = new Row(columns, rowValues);
+            Assert.Equal("Jacinto", testRow.GetValue("Name"));
+            Assert.Equal("37", testRow.GetValue("Age"));
+
+            testRow.SetValue("Name", "Maider");
+            Assert.Equal("Maider", testRow.GetValue("Name"));
+            Assert.Null(testRow.GetValue("Nombre"));
+        }
     }
 }
