87302c6b64904855499faae5e2179cbddf8cb95e
AITANA MARTINEZ DE MARIGORTA BUSTERO <amartinezdemar003@ikasle.ehu.eus>
2026-02-02 11:42:59 +0100

    close #22. Row Is True finished and tested

--- OurTests/TableTests.cs
+++ OurTests/TableTests.cs
@@ -4,14 +4,6 @@ namespace OurTests
 {
     public class TableTests
     {
-        //TODO DEADLINE 1A : Create your own tests for Table
-        /*
-        [Fact]
-        public void Test1()
-        {
-
-        }
-        */
         [Fact]
         public void ColumnIndexByNameTest()
         {
