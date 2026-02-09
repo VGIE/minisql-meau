1ce352cf17a76b1b2c79258932ab5f5eb29e12a9
mmateos0 <maialenmateos9@gmail.com>
2026-02-09 11:10:08 +0100

    #14 Completed methows of class dataBase including and tests: - DropTable. - InsertTable.

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -1,3 +1,6 @@
+using DbManager;
+using System.Collections.Generic;
+using DbManager.Parser;
 namespace OurTests
 {
     public class UnitTest1
@@ -10,5 +13,48 @@ namespace OurTests
 
         }
         */
+
+        /*DropTable - Maialen*/
+
+        [Fact]
+        public void DropTableEX()
+        {
+            Database testDataBase = Database.CreateTestDatabase();
+            string tableName = "TestTable";
+
+            Assert.NotNull(testDataBase.TableByName(tableName));
+            Assert.True(testDataBase.DropTable(tableName));
+            Assert.Equal(Constants.DropTableSuccess, testDataBase.LastErrorMessage);
+            Assert.Null(testDataBase.TableByName(tableName));
+        }
+
+        /*InsertTable - Maialen*/
+
+        [Fact]
+        public void TestInsertTrue()
+        {
+            Database testDatabase = Database.CreateTestDatabase();
+            string tableName = "TestTable";
+            List<string> row = new List<string> { "Maialen", "21", "1.54" };
+            Assert.True(testDatabase.Insert(tableName, row));
+            Assert.Equal(Constants.InsertSuccess, testDatabase.LastErrorMessage);
+            List<List<string>> expectedRows = new List<List<string>> { row };
+            testDatabase.CheckForTesting(tableName, expectedRows);
+        }
+
+        [Fact]
+        public void TestInsertMultipleRows()
+        {
+            Database testDatabase = Database.CreateTestDatabase();
+            string tableName = "TestTable";
+            List<List<string>> newRows = new List<List<string>>
+    {
+        new List<string> { "Unai", "21", "1.78" },
+        new List<string> { "Endika", "21", "1.80" }
+    };
+            testDatabase.AddTuplesForTesting(tableName, newRows);
+            Table t = testDatabase.TableByName(tableName);
+            Assert.True(t.NumRows() >= 2);
+        }
     }
 }
