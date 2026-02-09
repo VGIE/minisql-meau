22726c6d9fcda8b1246077fdfbc33a6954851327
Unai78o <unailobetepp@gmail.com>
2026-02-09 11:47:56 +0100

    #13, Database.CreateTable done and tested. Working properly.

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -1,4 +1,5 @@
 using DbManager;
+using System.Collections.Generic;
 
 namespace OurTests
 {
@@ -25,6 +26,30 @@ namespace OurTests
 
             Table ghostTable = db.TableByName("TablaInexistente");
             Assert.Null(ghostTable);
+        }
+
+        [Fact]
+        public void TestCreateTable()
+        {
+            Database db = Database.CreateTestDatabase();
+
+            List<ColumnDefinition> newCols =
+            [
+                new ColumnDefinition(ColumnDefinition.DataType.String, "Street")
+            ];
+
+            List<ColumnDefinition> noCols = [];
+
+            bool tableAlreadyExists = db.CreateTable("TestTable", newCols);
+            bool tableHasNoCols = db.CreateTable("TestTableNoCols", noCols);
+            bool tableCreated = db.CreateTable("PerfectTestTable", newCols);
+
+            Assert.False(tableAlreadyExists);
+            Assert.False(tableHasNoCols);
+            Assert.True(tableCreated);
+
+            Assert.NotNull(db.TableByName("PerfectTestTable"));
+            Assert.Equal("Street", db.TableByName("PerfectTestTable").GetColumn(0).Name);
         }
     }
 
