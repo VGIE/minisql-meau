d10c9f930ba02d1afd71e9cef9762a189d8f0c32
mmateos0 <maialenmateos9@gmail.com>
2026-02-12 12:09:46 +0100

    #14 test: fix failing ClassMethodTests for Deadline DatabaseClass

--- DBManager/Database.cs
+++ DBManager/Database.cs
@@ -94,6 +94,7 @@ namespace DbManager
         {
             //DEADLINE 1.B: Insert a new row to the table. If it doesn't exist return false and set LastErrorMessage appropriately
             //If everything goes ok, set LastErrorMessage with the appropriate success message (Check Constants.cs)
+            
             Table table = TableByName(tableName);
             
             if (table == null)
@@ -108,9 +109,13 @@ namespace DbManager
             {
                 LastErrorMessage=Constants.InsertSuccess;
                 return true;
+            }else
+            {
+                LastErrorMessage = Constants.ColumnCountsDontMatch;
+                return false;
             }
            
-            return false;
+            
         }
 
         public Table Select(string tableName, List<string> columns, Condition condition) // Endika
