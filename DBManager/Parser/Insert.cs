2237ff332b87942148832a4eac283454354ffe3d
Unai78o <unailobetepp@gmail.com>
2026-02-23 11:29:41 +0100

    #35, Insert constructor finished

--- DBManager/Parser/Insert.cs
+++ DBManager/Parser/Insert.cs
@@ -14,7 +14,8 @@ namespace DbManager
         public Insert(string table, List<string> values)
         {
             //TODO DEADLINE 2: Initialize member variables
-            
+            this.Table = table;
+            this.Values = values;
         }
 
         public string Execute(Database database)
