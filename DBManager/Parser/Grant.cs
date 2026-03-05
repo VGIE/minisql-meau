2f4182469f055b0c8871c540e09a6096ac7efbb9
Usuario <endika.ladislao@zaraobe.net>
2026-03-05 19:38:30 +0100

    Database tests passed

--- DBManager/Parser/Grant.cs
+++ DBManager/Parser/Grant.cs
@@ -25,7 +25,10 @@ namespace DbManager
         {
             //TODO DEADLINE 5: Run the query and return the appropriate message
             //UsersProfileIsNotGrantedRequiredPrivilege, SecurityProfileDoesNotExistError, PrivilegeDoesNotExistError, GrantPrivilegeSuccess, ProfileAlreadyHasPrivilege
-            
+            if (database == null)
+            {
+                return Constants.Error;
+            }
             return null;
             
         }
