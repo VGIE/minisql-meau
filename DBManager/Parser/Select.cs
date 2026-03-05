bbc6f1fd8377a4c38ca3337b685d79620f3bcab3
mmateos0 <maialenmateos9@gmail.com>
2026-03-05 09:43:49 +0100

    #38 feat: initialize Select class for parsing logic

--- DBManager/Parser/Select.cs
+++ DBManager/Parser/Select.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,9 @@ namespace DbManager
         public Select(string table, List<string> columns, Condition condition=null)
         {
             //TODO DEADLINE 2: Initialize member variables
-            
+            this.Table = table;
+            this.Columns = columns ?? new List<string> ();
+            this.Where = condition;
         }
 
         public string Execute(Database database)
