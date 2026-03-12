1a8b7c242a7825dab311882dfdca36c5fb292dbf
mmateos0 <maialenmateos9@gmail.com>
2026-03-12 12:39:04 +0100

    #33 DropTable constructor and execute of dropTable done

--- DBManager/Parser/DropTable.cs
+++ DBManager/Parser/DropTable.cs
@@ -13,7 +13,7 @@ namespace DbManager
         public DropTable(string table)
         {
             //TODO DEADLINE 2: Initialize member variables
-            
+            this.Table = table;
         }
 
         public string Execute(Database database)
@@ -21,7 +21,24 @@ namespace DbManager
             //TODO DEADLINE 3: Run the query and return the appropriate message
             //DropTableSuccess or the last error in the database
             
-            return null;
+            if(database == null)
+            {
+                return Constants.Error + "Database does not exist";
+            }
+
+            Table target = database.TableByName(Table);
+
+            if(target == null)
+            {
+                return Constants.TableDoesNotExistError;
+            }
+
+            if (database.DropTable(Table))
+            {
+                return Constants.DropTableSuccess;
+            }
+
+            return Constants.Error + "Couldn't drop table";
             
         }
     }
