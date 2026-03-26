44b1dc672a5131d63983662937085056903afa18
Usuario <endika.ladislao@zaraobe.net>
2026-03-26 10:51:30 +0100

    #39 Parser Delete and Update tests done

--- OurTests/parseTests/ParseDeleteTests.cs
+++ OurTests/parseTests/ParseDeleteTests.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Common;
 using DbManager;
+using NuGet.Frameworks;
 using Xunit;
 
 namespace OurTests.parseTests
 {
     public class ParseDeleteTests
     {
+        private Database db;
+        private List<ColumnDefinition> definitionColumn;
         [Fact]
         public void ParseDelete()
         {
+            db = new Database("admin", "1234");
+            definitionColumn = new List<ColumnDefinition>
+            {
+                new ColumnDefinition(ColumnDefinition.DataType.String, "Name"),
+                new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"),
+                new ColumnDefinition(ColumnDefinition.DataType.Double, "Height")
+            };
+            Table table = new Table("Users", definitionColumn);
+            var rowValues = new List<String> { "Ana", "20", "1.65" };
+            table.AddRow(new Row(definitionColumn, rowValues));
+            db.AddTable(table);
+        }
+        [Fact]
+        public void CorrectWithSpaces()
+        {
+            var query = MiniSQLParser.Parse("DELETE  FROM Table WHERE Col1 = 10") as DbManager.Parser.Delete;
+            Assert.NotNull(query);
+            Assert.Equal("Table", query.Table);
+        }
+        [Fact]
+        public void SimpleStringCondition()
+        {
+            var query = MiniSQLParser.Parse("DELETE FROM Table WHERE Col1 ='Val1'") as DbManager.Parser.Delete;
+            Assert.NotNull(query);
+        }
+        [Fact]
+        public void SimpleIntCondition()
+        {
+            var query = MiniSQLParser.Parse("DELETE FROM Table WHERE Col1 = 10") as DbManager.Parser.Delete;
+            Assert.NotNull (query);
+        }
+        [Fact]
+        public void SimpleDoubleCondition()
+        {
+            var query = MiniSQLParser.Parse("DELETE FROM Table WHERE Col1 = 10.5") as DbManager.Parser.Delete;
+            Assert.NotNull(query);
+        }
+        [Fact]
+        public void IncorrectSpaces()
+        {
+            Assert.Null(MiniSQLParser.Parse("DELETEFROM Table WHERE Col1 =10"));
+        }
+        [Fact]
+        public void IncorrectCapitalization()
+        {
+            Assert.Null(MiniSQLParser.Parse("Delete FROM Table WHERE Col1 = 10"));
 
         }
     }
---
+++ OurTests/parseTests/ParseUpdateTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using DbManager;
+using NuGet.Frameworks;
+using Xunit;
+
+namespace OurTests.parseTests
+{
+    public class ParseUpdateTests
+    {
+        private Database db;
+        private List<ColumnDefinition> definitionColumn;
+        [Fact]
+        public void ParseUpdateTest()
+        {
+            db = new Database("admin", "1234");
+            definitionColumn = new List<ColumnDefinition>
+            {
+                new ColumnDefinition(ColumnDefinition.DataType.String, "Name"),
+                new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"),
+                new ColumnDefinition(ColumnDefinition.DataType.Double, "Height")
+            };
+            Table table = new Table("Users", definitionColumn);
+            var rowValues = new List<String> {"Ana", "20", "1.65" };
+            table.AddRow(new Row(definitionColumn, rowValues));
+            db.AddTable(table);
+        }
+        [Fact]
+        public void CorrectWithSpaces()
+        {
+            var query = MiniSQLParser.Parse("UPDATE Table SET Col1='string1', Col2=10 WHERE Col3 = 'string3'") as Update;
+            Assert.NotNull(query);
+            Assert.Equal("Table", query.Table);
+            var query2 = MiniSQLParser.Parse("UPDATE  Table SET Col1='string1', Col2=10 WHERE Col3 = 'string3'") as Update;
+            Assert.NotNull(query2);
+        }
+        [Fact]
+        public void StringValue()
+        {
+            var query = MiniSQLParser.Parse("UPDATE Table SET Col1='string1' WHERE Col3 = 'string3'") as Update;
+            Assert.NotNull(query);
+            Assert.Equal("Table", query.Table);
+        }
+        [Fact]
+        public void IntValue()
+        {
+            var query = MiniSQLParser.Parse("UPDATE Table SET Col1=10 WHERE Col3 = 30") as Update;
+            Assert.NotNull(query);
+        }
+        [Fact]
+        public void DoubleValue()
+        {
+            var query = MiniSQLParser.Parse("UPDATE Table SET Col1=10.5 WHERE Col3 = 30.5") as Update;
+            Assert.NotNull(query);
+        }
+        [Fact]
+        public void IncorrectSpacesOrMissingApostrophes()
+        {
+            Assert.Null(MiniSQLParser.Parse("UPDATE Table SET Col1=Val1WHERE Col3=Val3"));
+            Assert.Null(MiniSQLParser.Parse("UPDATE Table SET Col1='Val1'WHERE Col3='Val3'"));
+        }
+        [Fact]
+        public void IncorrectWithoutSET()
+        {
+            Assert.Null(MiniSQLParser.Parse("UPDATE Table Col1='Val1"));
+        }
+        [Fact]
+        public void IncorrectCapitalization()
+        {
+            Assert.Null(MiniSQLParser.Parse("update Table SET Col1='Val1'"));
+        }
+    }
+}
