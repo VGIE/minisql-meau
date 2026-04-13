befcf5776db7b5dead188a450eb2124fd42730f4
AITANA MARTINEZ DE MARIGORTA BUSTERO <amartinezdemar003@ikasle.ehu.eus>
2026-04-13 13:01:08 +0200

    test isUserAdmin

--- OurTests/SecurityTests/ManagerTests.cs
+++ OurTests/SecurityTests/ManagerTests.cs
@@ -12,7 +12,34 @@ namespace OurTests.SecurityTests
 {
     public class ManagerTests
     {
-        
+        public void TestIsUserAdmin()
+        {
+            User admin = new User("admin", "1234");
 
+            Profile adminProfile = new Profile();
+            adminProfile.Name = Profile.AdminProfileName;
+            adminProfile.Users.Add(admin);
+
+            Manager managerAdmin = new Manager("admin");
+            managerAdmin.Profiles.Add(adminProfile);
+
+            Assert.True(managerAdmin.IsUserAdmin());
+
+            User user = new User("user", "1234");
+
+            Profile normalProfile = new Profile();
+            normalProfile.Name = "Users";
+            normalProfile.Users.Add(user);
+
+            Manager managerUser = new Manager("user");
+            managerUser.Profiles.Add(normalProfile);
+
+            Assert.False(managerUser.IsUserAdmin());
+        }
+
+       
     }
 }
+
+
+
